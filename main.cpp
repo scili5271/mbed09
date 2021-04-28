0663a1de7b3ee9f12f21b43ebaf1e07cb4d47df4
107061135 <s410067@bmsh.tn.edu.tw>
2021-04-28 03:11:38 -0700

    lab9

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "mbed.h"
+DigitalOut myled1(LED1);
+DigitalOut myled2(LED2);
+DigitalOut myled3(LED3);
+DigitalOut myled4(LED4);
+DigitalOut myled5(LED5);
+DigitalOut myled6(LED6);
+DigitalOut myled7(LED7);
+DigitalOut myled8(LED8);
+int main() {
+    myled1=1;
+    myled2=1;
+    myled3=1;
+    myled4=1;
+    myled5=1;
+    myled6=1;
+    myled7=1;
+    myled8=1;}
